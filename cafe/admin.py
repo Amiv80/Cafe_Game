9475a0dd426929b43b71618fbc7e7be887165a4c
Amiv80 <amir.r.v138@gmail.com>
2024-09-04 21:12:57 +0330

    t

--- cafe/admin.py
+++ cafe/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from .models import Member
 
 
+@admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ["name", "vip_member", "date_add"]
-    list_filter = ["vip_member"]
+    list_display = ('name', 'vip_member', 'date_add')
+    search_fields = ('name',)
 
 
 admin.site.register(Member, MemberAdmin)
