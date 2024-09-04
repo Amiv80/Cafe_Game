d1d7e2c99cb499196b14e220267e98fbd364826b
Amiv80 <amir.r.v138@gmail.com>
2024-09-04 21:16:44 +0330

    edit

--- cafe/admin.py
+++ cafe/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from .models import Member
+from django.apps import apps
 
-
-@admin.register(Member)
-class MemberAdmin(admin.ModelAdmin):
-    list_display = ('name', 'vip_member', 'date_add')
-    search_fields = ('name',)
-
-
-admin.site.register(Member, MemberAdmin)
+if not admin.site.is_registered(Member):
+    @admin.register(Member)
+    class MemberAdmin(admin.ModelAdmin):
+        list_display = ('name', 'vip_member', 'date_add')
+        search_fields = ('name',)
